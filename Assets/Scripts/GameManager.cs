e5b6984081a8a745db55c4c70f61d469f02d84d5
cowaberry <lordxiek@gmail.com>
2019-10-02 20:57:15 -0400

    Actors list

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
     bool playerTurn;
-    Actors[] actors;
+    List<Actors> actors;
 
     void Start()
     {
-        
+       actors = FindObjectsOfType<Actors>().ToList<Actors>();
     }
 
     // Update is called once per frame
